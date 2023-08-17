1cb6fbf3aac6b54bab7d0b2eb962fefd1fc9d1c3
w1ck-3d <142054364+w1ck-3d@users.noreply.github.com>
2023-08-17 21:58:18 +0200

    Allow for muliple simulations in one go

--- wright_fisher.py
+++ wright_fisher.py
@@ -1,25 +1,27 @@
-import random
+import numpy as np
 
 
 class WrightFisher:
     def __init__(
         self,
-        a: int = 3,  # initial amount of alleles of type a
-        N: int = 10,  # population size
+        N: int = 100,  # population size
+        a: float = 0.5,  # initial fraction of alleles of type a
         s: float = 0.0,  # selection coefficient
         mu_1: float = 0.0,  # probability of mutation from a to A
         mu_2: float = 0.0,  # probability of mutation from A to a
     ) -> None:
-        assert 0 <= a <= N
+        assert 0.0 <= a <= 1.0
         assert 0.0 <= mu_1 <= 1.0
         assert 0.0 <= mu_2 <= 1.0
 
-        self.t = 0  # generation
-        self.a = a
+        self.a = round(N * a)
         self.N = N
         self.s = s
         self.mu_1 = mu_1
         self.mu_2 = mu_2
+
+        self.t = 0  # generation
+        self.history = [self.a]
 
     @property
     def A(self):
@@ -30,21 +32,29 @@ class WrightFisher:
 
     def step(self):
         self.t += 1
+        k = self.a
+        s = self.s
+        N = self.N
+        mu_1 = self.mu_1
+        mu_2 = self.mu_2
+        psi = (k * (1 + s) * (1 - mu_1) + (N - k) * mu_2) / (k * (1 + s) + N - k)
+        self.a = round(np.random.binomial(self.N, psi))
+        self.history.append(self.a)
 
-        new_a = 0
-        prob_of_a_sampled = (
-            self.a * (1 + self.s) * (1 - self.mu_1) + (self.N - self.a) * self.mu_2
-        ) / (self.a * (1 + self.s) + self.N - self.a)
-        for _ in range(self.N):
-            if random.random() < prob_of_a_sampled:
-                new_a += 1
-        self.a = new_a
+    def get_history(self):
+        return self.history
 
 
-NUM_STEPS = 10
+SIMULATION_COUNT = 30
+GENERATION_COUNT = 100
+N = 1000
+initial_prob_a = 0.1  # initial probability of allele a
+s = 0.2  # selection coefficient
+u = 0.1  # probability of mutation from a to A
 
-wf = WrightFisher()
-
-for _ in range(NUM_STEPS):
-    wf.step()
-    print(wf)
+simulations = []
+for _ in range(SIMULATION_COUNT):
+    wf = WrightFisher(N, initial_prob_a, s, u)
+    for _ in range(GENERATION_COUNT):
+        wf.step()
+    simulations.append(wf.get_history())
